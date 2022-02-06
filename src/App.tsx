63ba61e02a3c6fe7498e13b120c7602b30ea7682
Raique Ramos <raiqueramos.dev@gmail.com>
2022-02-06 09:38:18 -0300

    1.0.0

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@chakra-ui/react";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <nav>
+      <Container 
+        w="100%" 
+        h="100vh"
+        >
+        <h1>Hello World</h1>
+      </Container>
+    </nav>
+  );
+}
